fa6e9fbb383c1473da8707d67202c74229334aa1
AaronDasani <aaron.mike2406@gmail.com>
2019-08-05 08:46:55 -0700

    update applicationRb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,5 @@ class ApplicationController < ActionController::Base
   # UPLOAD_URL = "#{REDMINE_URL}/uploads.json?key=#{API_KEY}"  
   
   
-  # LEGACY REDMINE
-  # REDMINE_URL = 'http://amtredmine-legacy'      # eg 'https://example.plan.io'
-  # API_KEY = 'caf9d2ad62abd55ce60fa8f71edfa23d076a2b6a'         # eg 'ffff0000eeee1111dddd2222cccc3333bbbb4444'
-  
-  #LIVE REDMINE
-  REDMINE_URL = 'http://amtredmine' 
-  API_KEY='a9db0739f22507135e46f301b87ace1ed13e80a2'
-  UPLOAD_URL = "#{REDMINE_URL}/uploads.json?key=#{API_KEY}"
+ 
 end
